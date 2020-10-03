157c5636520c2d89b57451b4adcaa7d1d5014b20
Adam Anderson <anderson.adam@gmail.com>
2020-10-03 14:49:03 -0700

    Removed Entity Framework project and added DB test project

--- 1Basic/SchoolProject/dbo/Tables/Students.sql
+++ 1Basic/SchoolProject/dbo/Tables/Students.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Students] (
     [ID]             INT            IDENTITY (1, 1) NOT NULL,
     [LastName]       NVARCHAR (MAX) NULL,
-    [FirstMidName]   NVARCHAR (MAX) NULL,
-    [EnrollmentDate] DATETIME       NOT NULL,
+    [FirstName]   NVARCHAR (MAX) NULL,
+    [EnrollmentDate] DATETIME2       NOT NULL,
     CONSTRAINT [PK_dbo.Students] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
